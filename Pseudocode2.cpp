3406b51d639f8a9b95e10a583de273881bc79e67
patelami3431 <31361623+patelami3431@users.noreply.github.com>
2017-09-21 13:57:36 -0500

    Update Pseudocode2.cpp

--- Pseudocode2.cpp
+++ Pseudocode2.cpp
@@ -11,7 +11,6 @@ Class Network
         int batch_size;
         int epochs;
         int layer_sizes[];          //array of layer sizes
-        int L;                      //number of layers i.e. sizeof(layer_sizes)
         vector<matrix> weights;
         vector<matrix> biases;
         vector<matrix> activations;
@@ -23,8 +22,8 @@ Class Network
     Methods:
         Constructor(requires eta, batch size, layer sizes, file names, and epochs provided by the ReadIn static member function)
         {
-            //Assign L, which is the number of layers. This is given by sizeof(layer sizes). 
-            //Weights, biases, activations, weighted inputs, nabla_w, nabla_b vectors resized to size L.
+            //Generate L, which is the number of layers. This is given by sizeof(layer sizes). 
+            //Weights, biases, activations, weighted inputs, nabla_w, nabla_b resized to size L.
             //Everything but the activation vector will have an effective size of L-1, as their first element will be left unused.
             Looping an index i from 1 to L
                 resize the ith element of the weights vector to be layer sizes at i by layer sizes at i-1
@@ -51,20 +50,44 @@ Class Network
         //backProp requires sigmoid_prime function, cost derivative function, activations and weighted inputs already been set
         void backProp()
         {
-            nabla_b at L = calculated error of the output layer using cost derivative function
+            nabla_b at L = calculated error of the output layer
             Looping an index i from L-1 to 1 (the remaining layers)
                 nabla_b at i = ((weights at i + 1 transposed)*(nabla_b at i + 1)) hadamard product with sigmoid_prime at i
+            Loop starts from i = 1 to i = L
+                inner loop which will go from 0 to all the nodes in the layer
+                  element in nabla_w matrix=(activations of the node from which the weight is coming from the (i-1) th layer)*(nabla_b value for that node)
+              //doubtful about this (Please chaeck)
         }
         
-        static void ReadIn();
+        static void ReadIn(istream &in)  //accepts input stream as a parameter
+        {
+            //accepts the values of all the hyperparameters, an integer array, name of the training datafile and also, name of the 
+            //truthdata file
+            //assigns the values to the respected parameters in the class
+        }
+        
+        //Stochastic_Gradient_Descent() or SGD() will be the function which will complete the required epochs and update the values of 
+        //weights and biases in the network. This will keep the average of nabla_w and nabla_b or in other words, the cost partials.
+        //An epoch will be said to complete once the complete dataset goes through the forwardpass() and then, the backprop()
+        void Stochastic_Gradient_Descent()
+        {
+            double Total_nabla_w;  //to store the total of cost partials of weights
+            double Total_nabla_b;  //to store the total of cost partials of biases 
+            //A loop starts for one epoch and goes till the total number of epochs entered by the user
+                //Total_nabla_w and Total_nabla_b are set to 0
+                //the mini_batch size from the dataset, as entered by the user, will be chosen
+                //Usually, we will need to randomize the data after choosing our batch. But, randomization is not needed now, because  
+                //the data is generated randomly.
+                
+                //An inner loop starts, which will go from the first data in the minibatch to the last dataof that minibatch
+                    //will call the fowardpass() and backprop() methods for each training data
+                    //updates the Total_nabla_w and Total_nabla_b values
+                
+                //Update Weights, according to the equation:
+                            //w += (eta/n)nabla_w
+                //Update biases
+                            //b += (eta/n)nabla_b
+        }
 };
 
-//Just realized we can make ReadIn() to be a static member function of the class Network. It can be called directly in the main(), 
-//before even declaring any object of the class because it is a static function. And, we can read in the required parameters and assign
-//the values before calling the constructor. So, the constructor doesn't need any parameter if we make ReadIn() a static member function 
-//of the class Network. //@Yon- YESS :D that's why i really wanted to make it a static method. It's a separate entity than any specific 
-                        //      instantiation but still retains it's class membership
-void main()
-{
-    Network::ReadIn();      //that's how we can call it before declaring the object of the class
-}
+
