4a841dac47080b8a98b8f18646883ebb5df6bcf5
yonatanmg <31772603+yonatanmg@users.noreply.github.com>
2017-10-01 11:58:30 -0500

    Create Nework.h

---
+++ Network Class/Nework.h
@@ -0,0 +1,48 @@
+#ifndef NETWORK_H
+#define NETWORK_H
+
+#include <vector>
+#include <matrix> //?
+using namespace std;
+
+class Network
+{
+	public:
+		Network		(istream& = cin);
+		Network		(const string&, const string&) //might not be necessary
+		Network		(const string&)
+		~Network	();
+		
+		void	readInit	(const string&);
+		void	readInit	();
+		
+		void	forwardProp	(int);
+		bool	backProp	(int);
+		void	SGD			();
+		void	update		();
+		void	train		();
+		
+		void	classify	(const string&) // or maybe (ifstream&);
+		bool	writeToFile	();
+		matrix&	hadamardProduct (matri
+	private:
+		double			learningRate;
+		int				batchSize;
+		int				epochs;
+		vector<int>		layerSizes;
+		
+		vector<matrix>	weights;
+		vector<matrix>	biases;
+		vector<matrix>	activations;
+		
+		vector<matrix>	errors;
+		vector<matrix>	sumNablaB;
+		vector<matrix>	sumNablaW;
+		
+		vector<int>		miniBatchIndices;
+		
+		ifstream		trainingDataInfile;
+		ifstream		expectedValuesInfile;
+};
+
+#endif
