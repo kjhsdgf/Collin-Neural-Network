4c154bd12743a048e382129727a2fc1096abcd9a
patelami3431 <31361623+patelami3431@users.noreply.github.com>
2017-10-01 21:07:22 -0500

    Update and rename Nework.h to Network.h

---
+++ Network Class/Network.h
@@ -0,0 +1,50 @@
+#ifndef NETWORK_H
+
+#define NETWORK_H
+
+#include <iostream>
+#include <fstream>
+#include <dlib\matrix.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+using namespace dlib;
+
+typedef matrix<double> Matrix;
+typedef std::vector<Matrix> Vector;
+
+class Network
+{
+	public:
+			Network			(istream& = cin);
+			Network			(const string&, const string&);
+			Network			(const string&);
+			~Network		();
+		void	train			();
+		void	classify		(const string&);
+		bool	writeToFile		()	const;
+		Matrix&	hadamardProduct		(Matrix M1, Matrix M2);
+		void	readInit		(const string&);
+		void	readInit		();
+		void	forwardProp		(int);
+		bool	backProp		(int);
+		void	SGD			();
+		void	update			();
+
+	private:
+		double				learningRate;
+		int				batchSize;
+		int				epochs;
+		std::vector<int>		layerSizes;
+		Vector				weights;
+		Vector				biases;
+		Vector				activations;
+		Vector				errors;
+		Vector				sumNablaB;
+		Vector				sumNablaW;
+		Vector				miniBatchIndices;
+		ifstream			trainingDataInfile;
+		ifstream			expectedValuesInfile;
+};
+#endif
