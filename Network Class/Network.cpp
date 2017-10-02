960b750d5deb794c6b745d33e199fe3a126ed11f
patelami3431 <31361623+patelami3431@users.noreply.github.com>
2017-10-02 03:13:44 -0500

    Create Network.cpp

---
+++ Network Class/Network.cpp
@@ -0,0 +1,57 @@
+#include "Network.h"
+
+bool Network::createNetworkFile() 	
+{
+	ofstream outfile;
+	std::vector<int>::iterator i1;
+	int i;
+	int j;
+	int k;
+	string fileName("Previous_Network_");
+	time_t _tm = time(NULL);
+	struct tm * curtime = localtime(&_tm);
+	fileName += asctime(curtime);
+	outfile.open(fileName, ios_base::out);
+	if (outfile.is_open())
+	{
+		cout << "\nCreating file " << fileName << ".txt" << endl;
+		outfile << trainingDataFilename << endl;
+		outfile << expectedValuesFilename << endl;
+		outfile << learningRate << endl;
+		outfile << batchSize << endl;				
+		outfile << epochs << endl;
+		outfile << numLayers << endl;
+		i1 = layerSizes.begin();
+		for (; i1 != layerSizes.end(); i1++)
+		{
+			outfile << (*i1) << " ";
+		}
+		outfile << endl;
+		j = 1;
+		while (j < numLayers)
+		{
+			outfile << "w " << j;
+			for (i = 0; i < weights[j].nr(); i++)
+			{
+				for (k = 0; k < weights[j].nc(); k++)
+					outfile << weights[j](i, k) << " ";
+				outfile << endl;
+			}
+			outfile << "b " << j;
+			for (i = 0; i < biases[j].nr(); i++)
+			{
+				outfile << biases[j](i, 1);
+				outfile << endl;
+			}
+		}
+		outfile.close();
+		return true;
+	}
+	else
+		return false;
+}
+
+const Matrix& activationFunction(const Matrix& weighted_inputs, const int Layer_Size)
+{
+	return sigmoid(weighted_inputs);
+}
