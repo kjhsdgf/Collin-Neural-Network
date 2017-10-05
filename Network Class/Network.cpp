b6c30fb8fda814f3edb14e3339669adfd6043f36
Slunchy Kjhsdgf <kjhsdgf@users.noreply.github.com>
2017-10-04 19:52:04 -0500

    Update Network.cpp

--- Network Class/Network.cpp
+++ Network Class/Network.cpp
@@ -136,7 +136,7 @@ const Matrix hadamardProduct(const Matrix &input_matrix_L, const Matrix &input_m
 
 //Updates weights and biases for the network by overwriting weights and biases.
 //helps if sumNablaW and sumNablaB VMatrix's have been filled.
-void update()
+void updateWeightsAndBiases()
 {
 	double k = (learningRate / batchSize); //saves (numLayers - 1) divisions
 	for (int i = 1; i < numLayers; i++)
