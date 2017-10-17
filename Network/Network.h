dac23b4d8b9fedc7673856f0f95e43af149d18c6
patelami3431 <31361623+patelami3431@users.noreply.github.com>
2017-10-16 22:35:34 -0500

    Update Network.h

--- Network/Network.h
+++ Network/Network.h
@@ -24,13 +24,13 @@ class Network
 public:
 
 	//**********Constructors and Destructors*******************
-						Network();
-						Network(const string&, const string&);	
-						Network(const string&);					
-						~Network();
+					Network();
+					Network(const string&, const string&);	
+					Network(const string&);					
+					~Network();
 
 	//**********Public Accessible Methods**********************
-	std::vector<double>	train();
+	std::vector<double>		train();
 	void				classify(const string&);
 
 	//***********Methods that read size of each layer, number of epochs, learning rate and the batch size for the network*********
@@ -42,8 +42,8 @@ private:
 
 	//Private Data Members:->
 	double				learningRate;
-	int					batchSize;
-	int					epochs;
+	int				batchSize;
+	int				epochs;
 	int 				numLayers;
 
 	VMatrix				weights;
@@ -56,7 +56,7 @@ private:
 
 	Vector				miniBatchIndices;
 	Vector				layerSizes;
-	std::vector<string>	wrongInputs;
+	std::vector<string>		wrongInputs;
 	string				trainingDataFilename;
 	string				expectedValuesFilename;
 	ifstream			trainingDataInfile;
@@ -69,7 +69,7 @@ private:
 	bool				writeToFile() const;
 	bool				backProp(int);
 	void				forwardProp(int, ifstream &);
-	int					SGD();
+	int				SGD();
 	layerReport			outputLayerReport();
 	void				checkLearningRate(int = 1);
 	void				checkEpochs();
@@ -77,21 +77,21 @@ private:
 	void				checkLayersString(string&);
 	void				checkNumLayers();
 	void				updateWeightsAndBiases();
-	int					filesize(istream&);
+	int				filesize(istream&);
 	bool				compareOutput(const Matrix&);
-	const Matrix		hadamardProduct(const Matrix&, const Matrix&);
+	const Matrix			hadamardProduct(const Matrix&, const Matrix&);
 
 	template <class T>
 	void				FYShuffle(std::vector<T>& v);
 
 	template<class T>
-	const matrix<T>		getM(ifstream&, int);	//A function to return a column matrix at any position i in the given file
+	const matrix<T>			getM(ifstream&, int);	//A function to return a column matrix at any position i in the given file
 
 	template<class T>
-	std::vector<T>		getV(ifstream&, int);
+	std::vector<T>			getV(ifstream&, int);
 
 	template <class T>
-	std::vector<T>		Strtok(const string& , char[]);
+	std::vector<T>			Strtok(const string& , char[]);
 	
 };
 
@@ -100,7 +100,7 @@ const Matrix activationFunction(const Matrix& weighted_inputs);
 const Matrix activationPrime(const Matrix &input_matrix);
 const Matrix costPrime(const Matrix &activations_vector, const Matrix &expected_vals_vector);
 const double distribution(const int num_neurons_in);
-	  void	 randomizeMatrix(Matrix &);
+      void   randomizeMatrix(Matrix &);
 
 //---------------------------------------------------------------------------------------------------
 
