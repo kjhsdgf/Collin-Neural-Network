9527803120eb9e63ece11144eb4ff4c330086a74
Slunchy Kjhsdgf <kjhsdgf@users.noreply.github.com>
2017-10-25 01:29:34 -0500

    Revert "Sandbox kjhsdgf new"

---
+++ Network Class/AuxiliaryMethods.cpp
@@ -0,0 +1,75 @@
+//PLEASE FOLLOW THIS: Tabs, 4, No wrap
+
+//Fisher Yates shuffle
+template <class T>
+void FYShuffle(vector<T>& v)
+{
+	//below is another method using shuffle defined in <algorithm> but it is potentially much slower and not much more random
+	//unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+	//shuffle(v.begin(), v.end(), default_random_engine(seed));
+
+	for (int i = v.size()-1; i > 0; i--)
+	{
+		int randI = rand() % i;
+		T temp = v[i];
+		v[i] = v[randI];
+		v[randI] = temp;
+	}
+}
+
+//Activation function: can be changed later by the programmer, however as we discussed reading a function from the file can be a little risky
+
+const Matrix activationFunction(const Matrix& weighted_inputs)
+{
+	return sigmoid(weighted_inputs);
+}
+
+//must srand(time(NULL)) before running
+//Uses polar form of Box-Muller transform of uniformly distributed variable in [0,1] to normally distributed 
+//variable in (-inf, inf). May end up simplified to exclude v2, but tbh I don't yet understand the transform.
+//Requires #include <math>
+const double distribution(const int numNeuronsIn)
+{
+	double v1, v2, w, z1;							//temp variables to hold 
+	double variance = (1 / sqrt(numNeuronsIn));	//standard deviation as per Nielsen's suggestion
+
+	//For some reason, I can't get the assignment to work right unless the operation is split into phases as shown.
+	//Should be something like v# = 2 * (std::rand() / RAND_MAX) - 1, but that causes stack overflow...
+	do {
+		v1 = std::rand();
+		v2 = std::rand();
+		v1 /= RAND_MAX;
+		v2 /= RAND_MAX;
+		v1 = 2 * v1 - 1;
+		v2 = 2 * v2 - 1;
+		w = v1 * v1 + v2 * v2;
+	} while (w >= 1);
+	w = sqrt((-2 * log(w)) / w);
+	z1 = v1 * w;
+
+	return (z1 * variance); //mean 0, standard deviation = variance
+}
+
+//Overwrites an input matrix's elements with random values produced by a distribution function
+//Loops over individual elements, since I haven't thought of a better way to accomplish this.
+//Requires #include <dlib/matrix.h>
+void randomizeMatrix(Matrix &inputMatrix)
+{
+		for (int i = 0; i < inputMatrix.nr(); i++)
+			for (int j = 0; j < inputMatrix.nc(); j++)
+				inputMatrix(i, j) = distribution(inputMatrix.nc());
+}
+
+//Returns the values for the first derivative of the sigmoid function at a given matrix's coordinates, passed as argument.
+//Expressed using the Hadamard product in source literature, but that class member may not be available to the user,
+//so I used pointwise_multiply() instead.
+const Matrix activationPrime(const Matrix &input_matrix)
+{
+	return pointwise_multiply(sigmoid(input_matrix), ones_matrix(input_matrix) - sigmoid(input_matrix));
+}
+
+//First derivative of the quadratic cost function
+const Matrix costPrime(const Matrix &activations_vector, const Matrix &expected_vals_vector)
+{
+	return (activations_vector - expected_vals_vector);
+}
