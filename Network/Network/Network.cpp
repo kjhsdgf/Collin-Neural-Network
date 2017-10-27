ab7c264c08b81fd71f9be153bbc75fb513a9c9da
kjhsdgf <kjhsdgf@gmail.com>
2017-10-27 02:23:19 -0500

    [Eli] This builds on my comp. Doesn't run.

--- Network/Network/Network.cpp
+++ Network/Network/Network.cpp
@@ -507,6 +507,7 @@ void Network::checkBatchSize()
 			continue;
 	}
 }
+
 void Network::checkLayersString(string& layer_string)
 {
 	//checks the string of layer sizes
@@ -893,6 +894,10 @@ bool Network::writeToFile() const
 	}
 }
 
+const Matrix Network::hadamardProduct(const Matrix &input_matrix_L, const Matrix &input_matrix_R)
+{
+	return pointwise_multiply(input_matrix_L, input_matrix_R);
+}
 
 // checks if Network output matches expected
 // @param - takes a matrix of expected vals (could be changed to vector though or process both if need be)
@@ -1065,3 +1070,72 @@ void Network::updateWeightsAndBiases()
 
 }
 
+template<class T>
+const matrix<T> Network::getM(ifstream& fin, const int i)
+{
+	std::vector<T> v;
+	matrix<T> m;
+	if (i >= 0)
+	{
+		fin.seekg(0);
+		string str;
+		for (int j = 0; j <= i; j++)
+			getline(fin, str);
+		getline(fin, str);
+		v = Strtok<T>(str, ",");
+		m.set_size(v.size(), 1);
+		for (int i = 0; i < v.size(); i++)
+		{
+			m(i, 0) = v[i];
+		}
+		return m;
+	}
+	else
+	{
+		cout << "\n Server error 403: Found Invalid Index" << endl;
+		m.set_size(0, 0);
+		return m;
+	}
+}
+
+template<class T>
+std::vector<T> Network::getV(ifstream& fin, const int i)
+{
+	std::vector<T> v;
+	if (i >= 0)
+	{
+		fin.seekg(0);
+		string str;
+		getline(fin, str);
+		fin.seekg(i * (str.size() + 2), ios_base::beg);
+		getline(fin, str);
+		v = Strtok<T>(str, ",");
+		return v;
+	}
+	else
+	{
+		cout << "\n Server error 404: Found Invalid Index" << endl;
+		v.resize(0);
+		return v;
+	}
+}
+
+//A Strtok(), which can help us assigning any vector later on while reading from a file
+//can be used in readInit() too
+//Considering the fact that we don't want any user or programmer to use it, Strtok<T> can be a private member of the class.
+template<class T>
+std::vector<T> Network::Strtok(const string &str, char Separator[])
+{
+	char * pN;
+	std::vector<T> v;
+	char *p = new char[str.size() + 1];
+	strcpy(p, str.c_str());
+	pN = strtok(p, Separator);
+	while (pN != NULL)
+	{
+		v.push_back(atof(pN));
+		pN = strtok(NULL, Separator);
+	}
+	delete[] p;
+	return v;
+}
