83f0eda6e49b4a83a847ae10af93f6961996c4c1
patelami3431 <31361623+patelami3431@users.noreply.github.com>
2017-10-30 22:46:34 -0500

    Update main.cpp

--- Network/Network/main.cpp
+++ Network/Network/main.cpp
@@ -2,23 +2,12 @@
 
 int main()
 {
-	/*string previousNetworkName = "C:\\Users\\Elijah\\source\\repos\\Collin-Neural-Network\\Network\\Network\\previousnetworkThu1305.txt";
+	string previousNetworkName = "C:\\Users\\Elijah\\source\\repos\\Collin-Neural-Network\\Network\\Network\\previousnetworkThu1305.txt";
 	Network brain(previousNetworkName);
 	string validationFileName = "C:\\Users\\Elijah\\source\\repos\\Collin-Neural-Network\\Data\\validationData.txt";
 	
 	//brain.train();
-	brain.classify(validationFileName);*/
-
-	Network N;
-	std::vector<double> v = N.train();
-	ofstream outfile;
-	outfile.open("Efficiency.txt", ios_base::out);
-	std::vector<double> ::const_iterator i1;
-	i1 = v.begin();
-	for (; i1 != v.end(); i1++)
-		outfile << (*i1) << '\n';
-	outfile.close();
-
+	brain.classify(validationFileName);
 
 	return 0;
 }
