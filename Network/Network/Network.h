7152f8702b98bb5903437423b78fd3e13fd3066b
patelami3431 <31361623+patelami3431@users.noreply.github.com>
2017-11-30 13:57:25 -0600

    Update Network.h

--- Network/Network/Network.h
+++ Network/Network/Network.h
@@ -9,6 +9,7 @@
 #include <ctype.h>
 #include <time.h>
 #include <vector>
+#include <math.h>
 #include <dlib\matrix\matrix_math_functions.h>
 
 
@@ -24,10 +25,10 @@ class Network
 public:
 
 	//**********Constructors and Destructors*******************
-						Network();
-						Network(const string&, const string&);
-						Network(const string&);
-						~Network();
+	Network();
+	Network(const string&, const string&);
+	Network(const string&);
+	~Network();
 
 	//**********Public Accessible Methods**********************
 	std::vector<double>	train();
@@ -46,12 +47,13 @@ public:
 	void				checkNumLayers();
 
 	//---------------------Methods to output the patterns generated--------------------------
-	void				displayActivations(const Matrix& , ostream&  = cout);
-	void				displayActivationPrimes(ostream& = cout);
+	void				displayActivations(const Matrix&, ostream& = cout);
 	void				createActivationsFile(const Matrix&);
-	
+
 	//Method to create the graph file for desired index:->
-	bool makeGraphFile(int , const string& = "None");
+	bool makeGraphFile(int , const string& = "None", int =0.7);
+	
+	bool setActivationFunc(int);
 
 	//----Enums
 	enum Inputs {
@@ -73,6 +75,22 @@ public:
 		numActivations
 	};
 
+	char activationFunc[numActivations][25] = {
+												"Linear",
+												"Sigmoid",
+												"Complementary Log-Log",
+												"Bipolar Sigmoid",
+												"Tanh",
+												"LeCun's Tanh", 
+												"Rectifier Linear",
+												"Smooth Rectifier",
+												"Logit",
+												"Softmax",
+												"RadialGaussian",
+												"Maxout",
+												"Leaky Rectifier Linear",
+												"Cosine"
+	};
 private:
 
 	//Private Data Members:->
@@ -97,6 +115,8 @@ private:
 	string				expectedValuesFilename;
 	ifstream			trainingDataInfile;
 	ifstream			expectedValuesInfile;
+	std::vector<std::vector<double>>	trainingData;
+	std::vector<std::vector<double>>	expectedValues;
 
 	struct 				layerReport { bool isAmbiguous; Matrix cleanOutput; };
 
@@ -104,41 +124,45 @@ private:
 	//Private Functions:->
 	bool				writeToFile() const;
 	bool				backProp(int);
-	void				forwardProp(int, ifstream &);
+	void				forwardProp(int, ifstream& );
 	int					SGD();
 	layerReport			outputLayerReport();
 	void				updateWeightsAndBiases();
-	int					filesize(istream&);
 	bool				compareOutput(const Matrix&);
 	const Matrix		hadamardProduct(const Matrix&, const Matrix&);
+	void				initTrainingData();
 
 	template <class T>
 	void				FYShuffle(std::vector<T>& v);
 
 	template<class T>
-	const matrix<T>			getM(ifstream&, int);	//A function to return a column matrix at any position i in the given file
+	const matrix<T>		getM(bool, int);	//A function to return a column matrix at any position i in the given file
 
 	template<class T>
-	std::vector<T>			getV(ifstream&, int);
+	std::vector<T>		getV(bool, int);
+
+	bool				getNext(std::vector<double>&, std::vector<double>&);
 
 	template <class T>
-	std::vector<T>			Strtok(const string&, char[]);
+	std::vector<T>		Strtok(const string&, char[]);
+	std::vector<double>	tokenize(const string&, char[] = ", ");
+
 
 	//Activation Methods:
-	 void		linear(int);
-	 void		Sigmoid(int);
-	 void		logLog(int);
-	 void		bipolarSigmoid(int);
-	 void		Tanh(int);
-	 void		LeCun_stanh(int);
-	 void		rectifier(int);
-	 void		smoothRectifier(int);
-	 void		logit(int);
-	 void		softmax(int);
-	 void		radialGaussian(int);
-	 void		maxout(int);
-	 void		leakyRelu(int);
-	 void		cosine(int);
+	void		linear(int);
+	void		Sigmoid(int);
+	void		logLog(int);
+	void		bipolarSigmoid(int);
+	void		Tanh(int);
+	void		LeCun_stanh(int);
+	void		rectifier(int);
+	void		smoothRectifier(int);
+	void		logit(int);
+	void		softmax(int);
+	void		radialGaussian(int);
+	void		maxout(int);
+	void		leakyRelu(int);
+	void		cosine(int);
 
 	//--------------------Declarations related to state table------------------------------------
 
@@ -147,17 +171,17 @@ private:
 	std::vector<string>		setActivations;
 
 	//Required Methods ->
-	void				Switch(unsigned char, int);
+	void				activationFuncSwitch(unsigned char, int);
 	void				initStateTable();
-	void				takeInput(int);
+	void				activationFuncSelect(int);
 };
 
 //---------Functions outside the class (Auxiliary functions)----------------------------------------
-const Matrix activationFunction(const Matrix& weighted_inputs);
-const Matrix activationPrime(const Matrix &input_matrix);
-const Matrix costPrime(const Matrix &activations_vector, const Matrix &expected_vals_vector);
-const double distribution(const int num_neurons_in);
-	  void   randomizeMatrix(Matrix &);
+//const Matrix	activationFunction(const Matrix& weighted_inputs);
+//const Matrix	activationPrime(const Matrix &input_matrix);
+const Matrix	costPrime(const Matrix &activations_vector, const Matrix &expected_vals_vector);
+const double	distribution(const int num_neurons_in);
+void			randomizeMatrix(Matrix &);
 
 //---------------------------------------------------------------------------------------------------
 
@@ -165,6 +189,56 @@ const double distribution(const int num_neurons_in);
 inline const Matrix Network::hadamardProduct(const Matrix &input_matrix_L, const Matrix &input_matrix_R)
 {
 	return pointwise_multiply(input_matrix_L, input_matrix_R);
+}
+
+inline void Network::linear(int index)
+{
+	activations[index] = weightedInputs[index];
+	activationPrime[index] = ones_matrix<double>(layerSizes[index], 1);
+}
+
+inline void Network::Sigmoid(int index)
+{
+	activations[index] = sigmoid(weightedInputs[index]);
+	activationPrime[index] = pointwise_multiply(activations[index], ones_matrix(activations[index]) - activations[index]);
+}
+
+inline void Network::Tanh(int index)
+{
+	activations[index] = tanh(weightedInputs[index]);
+	activationPrime[index] = ones_matrix<double>(layerSizes[index], 1) - squared(activations[index]);
+}
+
+inline void Network::LeCun_stanh(int index)
+{
+	//	1.7159 tanh((2/3) * weightedInputs(i,j)) 
+	activations[index] = (1.7159) * tanh((0.6666667) * weightedInputs[index]);
+	activationPrime[index] = 0.98143 * (ones_matrix<double>(layerSizes[index], 1) - (0.33964 * squared(activations[index])));
+}
+
+inline void Network::radialGaussian(int index)
+{
+	//	exp( -(1/2)*((weightedInputs(i,j)^2))
+	activations[index] = exp((-0.500) * (squared(weightedInputs[index])));
+	activationPrime[index] = pointwise_multiply(zeros_matrix<double>(layerSizes[index], 1) - weightedInputs[index], activations[index]);
+}
+
+inline void Network::cosine(int index)
+{
+	activations[index] = cos(weightedInputs[index]);
+	activationPrime[index] = (zeros_matrix<double>(layerSizes[index], 1) - sin(activations[index]));
+}
+
+template <class T>
+void Network::FYShuffle(std::vector<T>& v)
+{
+	for (int i = v.size() - 1; i > 0; i--)
+	{
+		int randI = std::rand() % i;
+		T temp = v[i];
+		v[i] = v[randI];
+		v[randI] = temp;
+	}
 }
 
 template <class T>
@@ -184,65 +258,23 @@ std::vector<T> Network::Strtok(const string& str, char Separator[])
 	return v;
 }
 
-template <class T>
-void Network::FYShuffle(std::vector<T>& v)
-{
-	for (int i = v.size() - 1; i > 0; i--)
-	{
-		int randI = std::rand() % i;
-		T temp = v[i];
-		v[i] = v[randI];
-		v[randI] = temp;
-	}
-}
-
-
 template<class T>
-const matrix<T> Network::getM(ifstream& fin, int i)
+const matrix<T> Network::getM(bool exp, int i)
 {
-	std::vector<T> v2;
-	matrix<T> m;
-	if (i >= 0)
-	{
-		fin.seekg(0);
-		string str;
-		for (int j = 0; j <= i; j++)
-			getline(fin, str);
-		v2 = Strtok<T>(str, ",");
-		m.set_size(v2.size(), 1);
-		for (int i = 0; i < v2.size(); i++)
-		{
-			m(i, 0) = v2[i];
-		}
-		return m;
-	}
-	else
-	{
-		cout << "\n Server error 403: Found Invalid Index" << endl;
-		m.set_size(0, 0);
-		return m;
-	}
+	std::vector<T> v = getV<T>(exp, i);
+	matrix<T> m; m.set_size(v.size(), 1);
+	for (int i = 0; i < v.size(); i++)
+		m(i, 0) = v[i];
+	return m;
 }
 
 template<class T>
-std::vector<T> Network::getV(ifstream& fin, int i)
+std::vector<T> Network::getV(bool exp, int i)
 {
-	std::vector<T> v;
-	if (i >= 0)
-	{
-		fin.seekg(0);
-		string str;
-		for (int j = 0; j <= i; j++)
-			getline(fin, str);
-		v = Strtok<T>(str, ",");
-		return v;
-	}
+	if (exp)
+		return expectedValues[i];
 	else
-	{
-		cout << "\n Server error 404: Found Invalid Index" << endl;
-		v.resize(0);
-		return v;
-	}
+		return trainingData[i];
 }
 
 #endif
